339fd4df07aa97f0acd3615cc0cfb2fdbab1bc91
unknown <nguruedwin46@gmail.com>
2025-08-26 21:22:39 +0300

    improved hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 export default function HeroSection() {
   return (
-    <section className="relative bg-gradient-to-r from-blue-600 via-indigo-700 to-purple-700 text-white overflow-hidden">
+    <section className="relative bg-gradient-to-r from-blue-950 via-indigo-800 to-purple-900 text-white overflow-hidden">
       {/* Background overlay */}
-      <div className="absolute inset-0 bg-black/40"></div>
+      <div className="absolute inset-0 bg-blac"></div>
 
-      <div className="relative z-10 max-w-7xl mx-auto px-6 md:px-8 lg:px-16 pt-20 flex flex-col md:flex-row items-center">
+      <div className="relative z-10 max-w-7xl mx-auto px-4 md:px-6 lg:px-14 py-20 flex flex-col md:flex-row gap-5 items-center">
         
         {/* Left Content */}
         <motion.div
@@ -18,8 +18,8 @@ export default function HeroSection() {
           className="text-center md:text-left md:w-1/2"
         >
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-extrabold leading-tight">
-            Experience God's Love{" "}
-            <span className="text-red-600">With Us</span>
+            Experience God's {" "}
+            <span className="text-yellow-400">Love With Us</span>
           </h1>
 
           <p className="mt-6 text-lg md:text-xl text-gray-200">
@@ -31,7 +31,7 @@ export default function HeroSection() {
           <div className="mt-8 flex flex-col sm:flex-row sm:justify-center md:justify-start gap-4">
             <Link
               href="/services"
-              className="px-6 py-3 bg-red-600 text-gray-100 rounded-2xl shadow-md font-semibold hover:bg-yellow-500 transition"
+              className="px-6 py-3 bg-yellow-400 text-indigo-900 rounded-2xl shadow-md font-semibold hover:bg-yellow-500 transition"
             >
               Watch Live
             </Link>
@@ -52,9 +52,9 @@ export default function HeroSection() {
           className="mt-12 md:mt-0 md:w-1/2 flex justify-center"
         >
           <img
-            src="/images/1754123364089-removebg-preview.png"
+            src="/images/1754123364089c.jpg"
             alt="Web & Graphics Illustration"
-            className="w-full max-w-md md:max-w-lg lg:max-w-xl drop-shadow-2xl"
+            className="w-full rounded-3xl max-w-md md:max-w-lg lg:max-w-xl drop-shadow-2xl"
           />
         </motion.div>
       </div>
