03a1d36021ae3aa5da82c3db0c59ec31b832ed29
unknown <nguruedwin46@gmail.com>
2025-08-26 23:32:21 +0300

    adjusted py in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
     }`;
 
   return (
-    <nav className="bg-white shadow-md sticky top-0 z-50">
+    <nav className="bg-white py-2 shadow-md sticky top-0 z-50">
       <div className="container mx-auto flex justify-between items-center px-4 py-4">
         <h1 className="text-xl md:text-2xl font-bold text-red-700">JCM</h1>
 
